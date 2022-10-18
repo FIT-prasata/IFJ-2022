161a016351b1c7fd8c8988bd5b2ecc60115565eb
Marek Prochazka <m.prochazka2002@gmail.com>
2022-10-18 09:51:11 +0200

    assert_true macro

--- src/__tests__/t_dynamic_string.c
+++ src/__tests__/t_dynamic_string.c
@@ -1,15 +1,20 @@
 #include "../dynamic_string.h"
 #include "test_utils.h"
 
-TEST(first_test, "dummy test to see if this works")
-result = PASSED;
-ENDTEST
+//TEST(first_test, "dummy test to see if this works")
+//ASSERT_TRUE(1 == 2)
+//ENDTEST
 
-TEST(second_test, "dummy test to see if this works")
-result = FAILED;
-ENDTEST
+//TEST(second_test, "dummy test to see if this works")
+//result = FAILED;
+//ENDTEST
+
+//TEST(d_string_init_and_free, "test d_string_init and d_string_free_and_clear")
+//DString_t d_string;
+//ASSERT_TRUE(d_string_init(&d_string)==OK);
+//ENDTEST
 
 void run_all_tests() {
-    first_test();
-    second_test();
+//    first_test();
+//    second_test();
 }
--- src/__tests__/test_utils.h
+++ src/__tests__/test_utils.h
@@ -3,20 +3,27 @@
 #include <assert.h>
 #include <stdio.h>
 
+#include "../error.h"
+
 typedef enum { PASSED, FAILED } TestResult_t;
 
 #define TEST(NAME, DESCRIPTION)       \
     void NAME() {                     \
         TestResult_t result = FAILED; \
         char* t_name = #NAME;         \
-        char* t_desc = #DESCRIPTION;
-#define ENDTEST                                               \
-    if (result == PASSED) {                                   \
-        printf("[PASSED] %s", t_name);                        \
-    } else {                                                  \
-        fprintf(stderr, "[FAILED] %s -> %s", t_name, t_desc); \
-    }                                                         \
-    printf("\n");                                             \
+        char* t_desc = #DESCRIPTION;  \
+        char* e_msg = "no error";
+#define ENDTEST                                                         \
+    if (result == PASSED) {                                             \
+        printf("[PASSED] %s", t_name);                                  \
+    } else {                                                            \
+        fprintf(stderr, "[FAILED] %s -> failed on: %s", t_name, e_msg); \
+    }                                                                   \
+    printf("\n");                                                       \
     }
 
-#define FAIL(REASON) printf("[FAILED] %s", REASON)
+#define ASSERT_TRUE(CONDITION) \
+    if (!(CONDITION)) {        \
+        result = FAILED;       \
+        e_msg = #CONDITION;    \
+    }
