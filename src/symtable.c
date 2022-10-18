225c9c30c549df889c2925ec16a8560bd487ab1b
LAPTOP-5LH8963R\adamm <adam.m.svetlik@gmail.com>
2022-10-18 21:56:10 +0200

    #46 polishing

--- src/symtable.c
+++ src/symtable.c
@@ -16,7 +16,7 @@
 
 #include "string.h"
 
-// Konstruktor tabulky - vytvoří a inicializuje tabulku
+// Hashtable constructor - initializes hashtable
 // Vrací ukazatel na tabulku při úspěchu, jinak NULL
 // PARAMS: n - velikost hashovací tabulky
 htab_t *htab_init(size_t n) {
@@ -40,27 +40,7 @@ htab_t *htab_init(size_t n) {
     return htab_table;
 }
 
-// Konstruktor prvku - vytvoří a inicializuje prvek
-// Vrací ukazatel na prvek při úspěchu, jiank NULL
-// PARAMS: key - klíč
-/*htab_item_t *htab_item_init(htab_key_t key) {
-    // Alokace paměti pro strukturu prvku
-    htab_item_t *item = malloc(sizeof(htab_item_t));
-    if (item == NULL) return NULL;
 
-    // Alokace paměti pro klíč
-    //char *tmp = malloc(sizeof(htab_key_t) * (strlen(key) + 1));
-    //if (tmp == NULL) return NULL;
-
-    // Inicializace hodnot
-    //strcpy(tmp, key);
-    item->type = UNKNOWN;
-    item->token.type = T_UNDEF;
-    item->token.attribute.string = NULL;
-    item->next = NULL;
-
-    return item;
-}*/
 
 // Hashovací funkce - její výsledek modulo arr_size určuje index do tabulky
 // PARAMS: str - řetězec
@@ -117,10 +97,6 @@ htab_item_t *htab_lookup_add(htab_t *t, Token_t *token) {
     if (item == NULL) {
         if (htab_insert_item(t, token) == false) return NULL;
 
-        // Volání resize při překročení AVG_LEN_MAX
-        // if (t->size / t->arr_size > AVG_LEN_MAX)
-        // htab_resize(t, 2 * t->arr_size);
-
         item = htab_find(t, key);
 
         return item;
@@ -160,9 +136,6 @@ bool htab_erase(htab_t *t, htab_key_t key) {
     free(item);
     t->size--;
 
-    // Volání resize při překročení AVG_LEN_MIN
-    // if (t->size / t->arr_size < AVG_LEN_MIN && t->arr_size > 1)
-    // htab_resize(t, t->arr_size / 2);
 
     return true;
 }
@@ -178,12 +151,6 @@ void htab_clear(htab_t *t) {
             htab_erase(t, item->token->attribute.string);
             item = item->next;
         }
-        if (size !=
-            t->arr_size)  // Provedl se resize - je potřeba procházet od začátku
-        {
-            i = -1;
-            size = t->arr_size;
-        }
     }
     t->size = 0;
 }
@@ -198,71 +165,6 @@ void htab_free(htab_t *t) {
     free(t);
 }
 
-// Změní velikost alokovaného pole a přesune položky z původních seznamů
-// PARAMS: t - hashovací tabulka
-//         newn - nová velikost hashovací tabulky
-void htab_resize(htab_t *t, size_t newn) {
-    if (newn < 1) {
-        fprintf(stderr, "Chyba pri zmene velikosti pole");
-    } else {
-        // Zásobník pro uložení klíčů a hodnot z tabulky
-        htab_item_t backup[t->size];
-        size_t backup_index = 0;
-
-        // Ukládání klíčů a hodnot do zásobníku
-        for (size_t i = 0; i < t->arr_size; i++) {
-            htab_item_t *item = t->arr_ptr[i];
-
-            while (item != NULL) {
-                backup[backup_index].token->attribute.string =
-                    malloc(strlen(item->token->attribute.string) + 1);
-                strcpy((char *)backup[backup_index].token->attribute.string,
-                       item->token->attribute.string);
-                backup[backup_index++].type = item->type;
-                backup[backup_index++].token = item->token;
-                item = item->next;
-            }
-        }
-
-        // Mazaní a uvolnění prvků v tabulce
-        htab_item_t *item2;
-        htab_item_t *toFree;
-
-        for (size_t i = 0; i < t->arr_size; i++) {
-            item2 = t->arr_ptr[i];
-
-            while (item2 != NULL) {
-                toFree = item2;
-                item2 = item2->next;
-                // free((char *)toFree->pair.key);
-                free(toFree);
-            }
-            t->arr_ptr[i] = NULL;
-        }
-
-        // Změna velikosti tabulky
-        t->arr_ptr = realloc(t->arr_ptr, newn * sizeof(htab_item_t *));
-        t->arr_size = newn;
-        t->size = 0;
-
-        // Inicializace nové tabulky
-        for (size_t i = 0; i < t->arr_size; i++) {
-            t->arr_ptr[i] = NULL;
-        }
-
-        // Nahrání klíčů a hodnot ze zásobníku do nové tabulky
-        for (size_t i = 0; i < backup_index; i++) {
-            htab_insert_item(t, backup[i].token);
-            htab_find(t, backup[i].token->attribute.string)->type =
-                backup[i].type;
-            htab_find(t, backup[i].token->attribute.string)->token =
-                backup[i].token;
-
-            // Uvolnění paměti zásobníku
-            // free((char *)backup[i].key);
-        }
-    }
-}
 
 void print_table(htab_t *t) {
     const char *enum_type[6] = {"INT",   "STRING", "BOOL",
