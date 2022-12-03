827eb1a9fe3d1f54035572b7e693dd09759037ea
Adam Světlík <adam.m.svetlik@gmail.com>
2022-12-03 12:17:10 +0100

    gitignore

--- src/main.c
+++ src/main.c
@@ -24,5 +24,76 @@ int line_num = 1;
 
 
 int main(void) {
-    return 0;
+    FILE *file = fopen("./src/adam-test.out", "w");
+    CHECK_NULL(file);
+    Token_t *token1 = malloc(sizeof(Token_t));
+    CHECK_NULL(token1);
+    token1->attribute.string = "Zadejte cislo pro vypocet faktorialu\n";
+    token1->type = T_STRING;
+
+    CHECK_OK(generate(PROLOG, NULL, NULL, NULL, 0, NO, file));
+    CHECK_OK(generate(WRITE, NULL, token1, NULL, 0, NO, file));
+
+    token1->attribute.string = "$a";
+    token1->type = T_ID;
+    Token_t *token2 = malloc(sizeof(Token_t));
+    CHECK_NULL(token2);
+    token2->attribute.string = "int";
+    token2->type = T_INT;
+    CHECK_OK(generate(DEFVAR, NULL, token1, NULL, 0, GF, file));
+    CHECK_OK(generate(READ, token1, token2, NULL, 0, GF, file));
+    token2->type = K_NULL;
+    CHECK_OK(generate(IF_EQ, NULL, token1, token2, 1, GF, file));
+    token1->attribute.string = "Chyba pri nacitani celeho cisla!\n";
+    token1->type = T_STRING;
+    CHECK_OK(generate(WRITE, NULL, token1, NULL, 0, NO, file));
+    CHECK_OK(generate(IF_ELSE, NULL, NULL, NULL, 1, NO, file));
+    CHECK_OK(generate(IF_END, NULL, NULL, NULL, 1, NO, file));
+    token1->attribute.string = "$a";
+    token1->type = T_ID;
+    token2->attribute.string = "0";
+    token2->type = T_INT;
+    CHECK_OK(generate(IF_LT, NULL, token1, token2, 2, GF, file));
+    token1->attribute.string = "Faktorial nelze spocitat\n";
+    token1->type = T_STRING;
+    CHECK_OK(generate(WRITE, NULL, token1, NULL, 0, NO, file));
+    CHECK_OK(generate(IF_ELSE, NULL, NULL, NULL, 2, NO, file));
+    token1->attribute.string = "$vysl";
+    token1->type = T_ID;
+    CHECK_OK(generate(DEFVAR, NULL, token1, NULL, 0, GF, file));
+    token2->attribute.string = "1";
+    token2->type = T_INT;
+    CHECK_OK(generate(ASSIGN, token1, token2, NULL, 0, GF, file));
+    token1->attribute.string = "$a";
+    token1->type = T_ID;
+    token2->attribute.string = "1";
+    token2->type = T_INT;
+    CHECK_OK(generate(WHILE_GT, NULL, token1, token2, 1, NO, file));
+    token1->attribute.string = "$vysl";
+    token1->type = T_ID;
+    token2->attribute.string = "$a";
+    token2->type = T_ID;
+    CHECK_OK(generate(MUL, token1, token1, token2, 0, GF, file));
+    token1->attribute.string = "$a";
+    token1->type = T_ID;
+    token2->attribute.string = "1";
+    token2->type = T_INT;
+    CHECK_OK(generate(SUB, token1, token1, token2, 0, GF, file));
+    CHECK_OK(generate(WHILE_END, NULL, NULL, NULL, 1, NO, file));
+    token1->attribute.string = "Vysledek je: ";
+    token1->type = T_STRING;
+    CHECK_OK(generate(WRITE, NULL, token1, NULL, 0, GF, file));
+    token1->attribute.string = "$vysl";
+    token1->type = T_ID;
+    CHECK_OK(generate(WRITE, NULL, token1, NULL, 0, GF, file));
+    token1->attribute.string = "\n";
+    token1->type = T_STRING;
+    CHECK_OK(generate(WRITE, NULL, token1, NULL, 0, NO, file));
+    CHECK_OK(generate(IF_END, NULL, NULL, NULL, 2, NO, file));
+
+    free(token1);
+    free(token2);
+    fclose(file);
+    // TODO: frame maybe as a parameter of token
+    return OK;
 }
