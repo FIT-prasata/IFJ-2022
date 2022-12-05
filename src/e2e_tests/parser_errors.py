17dbefa1752ecb5b7b7b80fd9e4a76db0b157ec3
Marek Prochazka <m.prochazka2002@gmail.com>
2022-12-05 19:07:42 +0100

    some exaple tests

--- src/e2e_tests/parser_errors.py
+++ src/e2e_tests/parser_errors.py
@@ -1,11 +1,100 @@
 from .utils import execute_programme
 import unittest
 
+
 class ParserTestCases(unittest.TestCase):
-    def test_missig_dollar_sign_in_variable(self):
-        stdin = """
-            <?php
+    def test_good_code(self):
+        """Tests that good code is accepted"""
+        stdin = """<?php
+        declare(strict_types=1);
+
+        $x = "Hello world";
+        """
+        self.assertEqual(execute_programme(stdin)[0], 0)
+
+    def test_missing_dollar_sign_in_variable(self):
+        stdin = """<?php
             declare(strict_types=1);
             x = "Hello world"
         """
         self.assertEqual(execute_programme(stdin)[0], 2)
+
+    def test_missing_semicolon(self):
+        stdin = """<?php
+            declare(strict_types=1);
+            $x = "Hello world"
+        """
+        self.assertEqual(execute_programme(stdin)[0], 2)
+
+    def test_wrong_function_definition(self):
+        stdin = """<?php
+            declare(strict_types=1);
+            function hello_world( {
+                $x = "Hello world";
+            }
+        """
+        self.assertEqual(execute_programme(stdin)[0], 2)
+
+    def test_wrong_function_definition_2(self):
+        stdin = """<?php
+            declare(strict_types=1);
+            function hello_world) {
+                $x = "Hello world";
+            }
+        """
+        self.assertEqual(execute_programme(stdin)[0], 2)
+
+    def test_wrong_function_definition_3(self):
+        stdin = """<?php
+            declare(strict_types=1);
+            function hello_world(x) {
+                $x = "Hello world";
+            }
+        """
+        self.assertEqual(execute_programme(stdin)[0], 2)
+
+    def test_proper_function_definition(self):
+        stdin = """<?php
+            declare(strict_types=1);
+            
+            function foo(string $param):string {
+                return bar($param);
+                }
+        """
+        self.assertEqual(execute_programme(stdin)[0], 0)
+
+    def test_function_call(self):
+        stdin = """<?php
+            declare(strict_types=1);
+            
+            function foo(string $param):string {
+                return bar($param);
+                }
+            
+            foo("Hello world");
+        """
+        self.assertEqual(execute_programme(stdin)[0], 0)
+
+    def test_factorial_code(self):
+        stdin = """<?php
+                    declare(strict_types=1);
+                    // Program 1: Vypocet faktorialu (iterativne)
+                    // Hlavni telo programu
+                    write("Zadejte cislo pro vypocet faktorialu\n");
+                    $a = readi();
+                    if ($a === null) {
+                    write("Chyba pri nacitani celeho cisla!\n");
+                    } else {}
+                    if ($a < 0) {
+                    write("Faktorial nelze spocitat\n");
+                    } else {
+                    $vysl = 1;
+                    while ($a > 0) {
+                    11
+                    $vysl = $vysl * $a;
+                    $a = $a - 1;
+                    }
+                    write("Vysledek je: ", $vysl, "\n");
+                    }
+        """
+        self.assertEqual(execute_programme(stdin)[0], 0)
--- src/e2e_tests/utils.py
+++ src/e2e_tests/utils.py
@@ -5,6 +5,8 @@ PROGRAMME_PATH = './main'
 def execute_programme(stdin:str) -> Tuple[int, str, str]:
     """Executes the programme with the given input and returns (status code, stdout, stderr)"""
     import subprocess
-    process = subprocess.Popen(PROGRAMME_PATH, stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+    # runs process and saves stdin and stdout to variables
+    process = subprocess.Popen([PROGRAMME_PATH], stdout=subprocess.PIPE, stdin=subprocess.PIPE, stderr=subprocess.PIPE)
     stdout, stderr = process.communicate(stdin.encode())
+
     return process.returncode, stdout.decode() if stdout else '', stderr.decode() if stderr else ''
